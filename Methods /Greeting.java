1ef75922683bc395b4f3dc6a8ba21c70dcff22fa
AparnaAppachikumar <120768010+AparnaAppachikumar@users.noreply.github.com>
2023-06-18 12:22:03 +0530

    Create Greeting.java

---
+++ Methods /Greeting.java
@@ -0,0 +1,10 @@
+import java.util*;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting() {
+        System.out.println("Hello World");
+    }
+}
